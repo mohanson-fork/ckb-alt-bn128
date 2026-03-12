02318e1f5e4b2b4f2dd311aa5929fd0c30dab536
mohanson <mohanson@outlook.com>
2026-03-12 13:59:02 +0800

    Bump version to v0.1.1

--- build.rs
+++ build.rs
@@ -1,22 +1,31 @@
+use std::io;
+
+fn show_clang_version(clang: &str) -> io::Result<std::process::Output> {
+    std::process::Command::new(clang).arg("--version").output()
+}
+
 fn find_clang() -> String {
     if let Ok(clang) = std::env::var("CLANG") {
-        return clang;
-    }
-    for version in [22, 21, 20, 19, 30, 29, 28, 27, 26, 25, 24, 23] {
-        let clang = format!("clang-{}", version);
-        if std::process::Command::new(&clang)
-            .arg("--version")
-            .output()
-            .is_ok()
-        {
+        let clang_version = show_clang_version(&clang);
+        if clang_version.is_ok() {
+            let s = String::from_utf8_lossy(&clang_version.unwrap().stdout).to_string();
+            println!("cargo:warning={}", s);
             return clang;
         }
     }
-    if std::process::Command::new("clang")
-        .arg("--version")
-        .output()
-        .is_ok()
-    {
+    for version in [22, 21, 20, 19, 30, 29, 28, 27, 26, 25, 24, 23] {
+        let clang = format!("clang-{}", version);
+        let clang_version = show_clang_version(&clang);
+        if clang_version.is_ok() {
+            let s = String::from_utf8_lossy(&clang_version.unwrap().stdout).to_string();
+            println!("cargo:warning={}", s);
+            return clang;
+        }
+    }
+    let clang_version = show_clang_version("clang");
+    if clang_version.is_ok() {
+        let s = String::from_utf8_lossy(&clang_version.unwrap().stdout).to_string();
+        println!("cargo:warning={}", s);
         return "clang".to_string();
     }
     panic!(
