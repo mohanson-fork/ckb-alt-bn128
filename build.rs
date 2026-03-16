99599fbad817d3aea903dcc9785f769872b63a0b
mohanson <mohanson@outlook.com>
2026-03-16 10:54:04 +0800

    Bump version to v0.1.3

--- build.rs
+++ build.rs
@@ -1,27 +1,45 @@
+use std::io;
+use std::process::Output;
+
+fn show_clang_version(clang: &str) -> io::Result<Output> {
+    std::process::Command::new(clang).arg("--version").output()
+}
+
 fn find_clang() -> String {
+    // The 1st, check if the CLANG environment variable is set and valid.
     if let Ok(clang) = std::env::var("CLANG") {
+        match show_clang_version(&clang) {
+            Ok(ok) => println!("{}", String::from_utf8_lossy(&ok.stdout)),
+            Err(_) => panic!(
+                "Clang compiler not found. Error CLANG environment: {}",
+                clang
+            ),
+        }
         return clang;
     }
+
+    // The 2nd, try clang-22, clang-21, ..., clang-19 in order.
     for version in [22, 21, 20, 19, 30, 29, 28, 27, 26, 25, 24, 23] {
         let clang = format!("clang-{}", version);
-        if std::process::Command::new(&clang)
-            .arg("--version")
-            .output()
-            .is_ok()
-        {
-            return clang;
+        match show_clang_version(&clang) {
+            Ok(ok) => {
+                println!("{}", String::from_utf8_lossy(&ok.stdout));
+                return clang;
+            }
+            Err(_) => continue,
         }
     }
-    if std::process::Command::new("clang")
-        .arg("--version")
-        .output()
-        .is_ok()
-    {
-        return "clang".to_string();
+
+    // The 3rd, try clang.
+    match show_clang_version("clang") {
+        Ok(ok) => {
+            println!("{}", String::from_utf8_lossy(&ok.stdout));
+            return "clang".to_string();
+        }
+        Err(_) => panic!(
+            "Clang compiler not found. Please set the CLANG environment variable to the path of your clang executable."
+        ),
     }
-    panic!(
-        "Clang compiler not found. Please set the CLANG environment variable to the path of your clang executable."
-    );
 }
 
 fn main() {
